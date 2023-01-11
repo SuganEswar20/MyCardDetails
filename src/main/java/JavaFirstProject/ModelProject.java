112886395d879083f7dfc24e3e69f0a67fdb490b
SuganEswar20 <sugavaneshwaran134@gmail.com>
2023-01-11 11:18:08 +0530

    Card Details

---
+++ src/main/java/JavaFirstProject/ModelProject.java
@@ -0,0 +1,19 @@
+package JavaFirstProject;
+
+public class ModelProject
+{
+    public static void main(String[] args)
+    {
+        long MyDocumentNumber=012345675l;
+        String HolderName="Sugan";
+        String FatherName="Poosanam";
+        String DateOfBirth="24/10/1999";
+        String Address="24 Car Street," + "Salem-0";
+        System.out.println("My Document No : "+MyDocumentNumber);
+        System.out.println("HolderName : "+HolderName);
+        System.out.println("Holder's Father Name : "+FatherName);
+        System.out.println("Holder Date Of Birth : "+DateOfBirth);
+        System.out.println("Holder Address : "+Address);
+
+    }
+}

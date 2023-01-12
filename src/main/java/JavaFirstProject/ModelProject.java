a7363ccce2168744af4f8c9aa34a461c82a0f90b
SuganEswar20 <sugavaneshwaran134@gmail.com>
2023-01-12 13:31:51 +0530

    Card Details

--- src/main/java/JavaFirstProject/ModelProject.java
+++ src/main/java/JavaFirstProject/ModelProject.java
@@ -5,11 +5,13 @@ public class ModelProject
     public static void main(String[] args)
     {
         long MyDocumentNumber=12345675L;
+        float MyPercentage=96.87f;
         long MobileNumber=6374734318L;
         String HolderName="Sugan";
         String FatherName="Poosanam";
         String DateOfBirth="24/10/1999";
         String Address="24 Car Street," + "Salem-0";
+        System.out.println("My Percentage : "+MyPercentage);
         System.out.println("My Mobile No : "+MobileNumber);
         System.out.println("My Document No : "+MyDocumentNumber);
         System.out.println("HolderName : "+HolderName);
